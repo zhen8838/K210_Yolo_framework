af8833a16eda98ff3f3a10b927c11c28c3e80875
郑启航 <597323109@qq.com>
2019-11-26 18:34:17 +0800

    resize success

--- tools/retinaface.py
+++ tools/retinaface.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
 from numpy import random
+import imgaug as ia
+import imgaug.augmenters as iaa
 import cv2
 from tools.yolo import center_to_corner, bbox_iou
 from tools.base import BaseHelper
+from typing import List, Iterable, Tuple
 
 
 def encode(matches, anchors, variances):
@@ -56,28 +59,34 @@ class RetinaFaceHelper(BaseHelper):
         self.val_epoch_step: int = None
         self.test_epoch_step: int = None
 
-        img_ann_list = np.load(image_ann, allow_pickle=True)
+        img_ann_list = np.load(image_ann, allow_pickle=True)[()]
 
-        if isinstance(img_ann_list[()], dict):
-            # NOTE can use dict set trian and test dataset
-            self.train_list = img_ann_list[()]['train']  # type:np.ndarray
-            self.val_list = img_ann_list[()]['val']  # type:np.ndarray
-            self.test_list = img_ann_list[()]['test']  # type:np.ndarray
-        elif isinstance(img_ann_list[()], np.ndarray):
-            self.train_list, self.val_list, self.test_list = np.split(
-                img_ann_list,
-                [int((1 - self.validation_split) * len(img_ann_list)),
-                    int((1 - self.validation_split / 2) * len(img_ann_list))])
-        else:
-            raise ValueError(f'{image_ann} data format error!')
-        self.train_total_data = len(self.train_list)
-        self.val_total_data = len(self.val_list)
-        self.test_total_data = len(self.test_list)
+        # NOTE can use dict set trian and test dataset
+        self.train_list: Iterable[Tuple[np.ndarray, np.ndarray]] = img_ann_list['train']
+        self.val_list: Iterable[Tuple[np.ndarray, np.ndarray]] = img_ann_list['val']
+        self.test_list: Iterable[Tuple[np.ndarray, np.ndarray]] = img_ann_list['test']
+        self.train_total_data: int = len(self.train_list)
+        self.val_total_data: int = len(self.val_list)
+        self.test_total_data: int = len(self.test_list)
 
         self.anchors = self._get_anchors(in_hw, anchor_min_size, anchor_steps)
-        self.in_hw: np.ndarray = in_hw
+        self.org_in_hw: np.ndarray = np.array(in_hw)
+        self.in_hw = tf.Variable(self.org_in_hw, trainable=False)
         self.pos_thresh: float = pos_thresh
         self.variances: np.ndarray = variances
+
+        self.iaaseq = iaa.Sequential([
+            iaa.Fliplr(0.5),
+            iaa.SomeOf([1, 4], [
+                iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                           backend='cv2', order=[0, 1], cval=(0, 255), mode=ia.ALL),
+                iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                           backend='cv2', order=[0, 1], cval=(0, 255), mode=ia.ALL),
+                iaa.Affine(rotate=(-30, 30),
+                           backend='cv2', order=[0, 1], cval=(0, 255), mode=ia.ALL),
+                iaa.Crop(percent=([0.05, 0.1], [0.05, 0.1], [0.05, 0.1], [0.05, 0.1]))
+            ], True)
+        ])  # type: iaa.meta.Sequential
 
     @staticmethod
     def _get_anchors(image_size=[640, 640],
@@ -108,30 +117,33 @@ class RetinaFaceHelper(BaseHelper):
         return anchors
 
     @staticmethod
-    def _crop_with_constraints(img, bbox, landm, clses, in_hw):
+    def _crop_with_constraints(img: np.ndarray,
+                               ann: List[np.ndarray],
+                               in_hw: np.ndarray
+                               ) -> [np.ndarray, List[np.ndarray]]:
         """ random crop with constraints
 
             make sure that the cropped img contains at least one face > 16 pixel at training image scale
         """
-        height, width, _ = img.shape
+        bbox, landm, clses = ann
+        im_h, im_w, _ = img.shape
         in_h, in_w = in_hw
 
         for _ in range(250):
-            PRE_SCALES = [0.3, 0.45, 0.6, 0.8, 1.0]
-            scale = random.choice(PRE_SCALES)
-            # short_side = min(width, height)
-            w = int(scale * width)
-            h = int(scale * height)
+            scale = random.choice([0.3, 0.45, 0.6, 0.8, 1.0])
+            # short_side = min(im_w, im_h)
+            new_w = int(scale * im_w)
+            new_h = int(scale * im_h)
 
-            if width == w:
+            if im_w == new_w:
                 l = 0
             else:
-                l = random.randint(width - w)
-            if height == h:
+                l = random.randint(im_w - new_w)
+            if im_h == new_h:
                 t = 0
             else:
-                t = random.randint(height - h)
-            roi = np.array((l, t, l + w, t + h))
+                t = random.randint(im_h - new_h)
+            roi = np.array((l, t, l + new_w, t + new_h))
 
             value = bbox_iof(bbox, roi[np.newaxis])
             flag = (value >= 1)
@@ -142,8 +154,7 @@ class RetinaFaceHelper(BaseHelper):
             mask_a = np.logical_and(roi[:2] < centers, centers < roi[2:]).all(axis=1)
             bbox_t = bbox[mask_a].copy()
             clses_t = clses[mask_a].copy()
-            landm_t = landm[mask_a].copy()
-            landm_t = landm_t.reshape([-1, 5, 2])
+            landm_t = landm[mask_a].copy().reshape([-1, 5, 2])
 
             if bbox_t.shape[0] == 0:
                 continue
@@ -162,8 +173,8 @@ class RetinaFaceHelper(BaseHelper):
             landm_t = landm_t.reshape([-1, 10])
 
             # make sure that the cropped img contains at least one face > 16 pixel at training image scale
-            b_w_t = (bbox_t[:, 2] - bbox_t[:, 0] + 1) / w * in_w
-            b_h_t = (bbox_t[:, 3] - bbox_t[:, 1] + 1) / h * in_h
+            b_w_t = (bbox_t[:, 2] - bbox_t[:, 0] + 1) / new_w * in_w
+            b_h_t = (bbox_t[:, 3] - bbox_t[:, 1] + 1) / new_h * in_h
             mask_b = np.minimum(b_w_t, b_h_t) > 0.0
             bbox_t = bbox_t[mask_b]
             clses_t = clses_t[mask_b]
@@ -172,11 +183,18 @@ class RetinaFaceHelper(BaseHelper):
             if bbox_t.shape[0] == 0:
                 continue
 
-            return image_t, bbox_t, landm_t, clses_t
+            return image_t, [bbox_t, landm_t, clses_t]
 
-        return img, bbox, landm, clses
+        return img, [bbox, landm, clses]
 
-    def reszie_img(self, img, bbox, landm, clses, in_hw):
+    def read_img(self, img_path: str) -> np.ndarray:
+        return cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
+    def resize_img(self, img: np.ndarray,
+                   ann: List[np.ndarray],
+                   in_hw: np.ndarray
+                   ) -> [np.ndarray, List[np.ndarray]]:
+        bbox, landm, clses = ann
         im_in = np.zeros((in_hw[0], in_hw[1], 3), np.uint8)
 
         """ transform factor """
@@ -185,30 +203,52 @@ class RetinaFaceHelper(BaseHelper):
         in_wh = in_hw[::-1]
         scale = np.min(in_hw / img_hw)
 
-        # NOTE hw_off is [h offset,w offset]
-        hw_off = ((in_hw - img_hw * scale) / 2).astype(int)
+        # NOTE calc the x,y offset
+        y_off, x_off = ((in_hw - img_hw * scale) / 2).astype(int)
         img = cv2.resize(img, None, fx=scale, fy=scale,
                          interpolation=np.random.choice(
                              [cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA,
                               cv2.INTER_NEAREST, cv2.INTER_LANCZOS4]))
 
-        im_in[hw_off[0]:hw_off[0] + img.shape[0],
-              hw_off[1]:hw_off[1] + img.shape[1], :] = img[...]
+        im_in[y_off:y_off + img.shape[0],
+              x_off:x_off + img.shape[1], :] = img[...]
 
         """ calc the point transform """
-        bbox[:, 0::2] = bbox[:, 0::2] * scale + hw_off[1]
-        bbox[:, 1::2] = bbox[:, 1::2] * scale + hw_off[0]
-        landm[:, 0::2] = landm[:, 0::2] * scale + hw_off[1]
-        landm[:, 1::2] = landm[:, 1::2] * scale + hw_off[0]
+        bbox[:, 0::2] = bbox[:, 0::2] * scale + x_off
+        bbox[:, 1::2] = bbox[:, 1::2] * scale + y_off
+        landm[:, 0::2] = landm[:, 0::2] * scale + x_off
+        landm[:, 1::2] = landm[:, 1::2] * scale + y_off
 
-        return im_in, bbox, landm, clses
+        return im_in, [bbox, landm, clses]
 
-    def resize_train_img(self, img, bbox, landm, clses, in_hw):
-        img, bbox, landm, clses = self._crop_with_constraints(img, bbox, landm, clses, in_hw)
-        return self.reszie_img(img, bbox, landm, clses)
+    def resize_train_img(self, img: np.ndarray,
+                         ann: List[np.ndarray],
+                         in_hw: np.ndarray
+                         ) -> [np.ndarray, List[np.ndarray]]:
+        img, ann = self._crop_with_constraints(img, ann, in_hw)
+        return self.resize_img(img, ann, in_hw)
+
+    def augment_img(self, img: np.ndarray,
+                    ann: List[np.ndarray],
+                    ) -> [np.ndarray, List[np.ndarray]]:
+        bbox, landm, clses = ann
+        bbs = ia.BoundingBoxesOnImage.from_xyxy_array(bbox, shape=img.shape)
+        kps = ia.KeypointsOnImage.from_xy_array(landm.reshape(-1, 2), shape=img.shape)
+
+        image_aug, bbs_aug, kps_aug = self.iaaseq(image=img, bounding_boxes=bbs,
+                                                  keypoints=kps)
+        bbs_aug = bbs_aug.remove_out_of_image().clip_out_of_image()
+
+        new_bbox = bbs_aug.to_xyxy_array()
+        new_landm = np.reshape(kps_aug.to_xy_array(), (-1, 5, 2))
+
+        return image_aug, new_bbox, new_landm, clses
 
     @staticmethod
-    def match(bbox, clses, landms, anchors, pos_thresh, variances):
+    def match(ann: List[np.ndarray],
+              anchors: np.ndarray, pos_thresh: float, variances: list
+              ) -> List[np.ndarray]:
+        bbox, clses, landms = ann
         overlaps = bbox_iou(bbox, center_to_corner(anchors, False))
         best_prior_overlap = np.max(overlaps, 1)
         best_prior_idx = np.argmax(overlaps, 1)
@@ -235,26 +275,24 @@ class RetinaFaceHelper(BaseHelper):
         return loc, landm, conf
 
     def process_img(self, img: np.ndarray, ann: np.ndarray, in_hw: np.ndarray,
-                    is_augment: bool, is_resize: bool, is_normlize: bool):
+                    is_augment: bool, is_resize: bool, is_normlize: bool
+                    ) -> [np.ndarray, List[np.ndarray]]:
         bbox, landm, clses = np.split(ann, [4, -1], 1)
         if is_resize and is_augment:
-            img, bbox, landm, clses = self.resize_train_img(img, bbox, landm, clses, in_hw)
+            img, ann = self.resize_train_img(img, [bbox, landm, clses], in_hw)
         elif is_resize:
-            img, bbox, landm, clses = self.resize_img(img, bbox, landm, clses, in_hw)
+            img, ann = self.resize_img(img, ann, in_hw)
         if is_augment:
             img, ann = self.augment_img(img, ann)
         if is_normlize:
             img = self.normlize_img(img)
         return img, ann
 
-    def ann_to_train_label(self, ann: np.ndarray):
-        bbox = ann[:, :4]
-        landms = ann[:, 4:14]
-        clses = ann[:, -1]
-        label_loc, label_landm, label_conf = self.match(bbox, clses, landms,
+    def ann_to_label(self, ann: List[np.ndarray]) -> List[np.ndarray]:
+        label_loc, label_landm, label_conf = self.match(ann,
                                                         self.anchors, self.pos_thresh,
                                                         self.variances)
-        return label_loc, label_conf, label_landm
+        return label_loc, label_landm, label_conf
 
     def build_datapipe(self, image_ann_list: np.ndarray, batch_size: int,
                        is_augment: bool, is_normlize: bool,
